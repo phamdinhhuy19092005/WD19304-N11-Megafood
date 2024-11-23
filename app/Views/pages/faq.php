84ef00bc5549db00854ed26f84786cc8281f4b7d
Huỳnh Hoàng Hiếu <hieuhhps38932@gmail.com>
2024-11-23 15:16:53 +0700

    add file faq php

---
+++ app/Views/pages/faq.php
@@ -0,0 +1,193 @@
+<div class="bodywrap">
+        <section class="bread_crumb">
+            <div class="container_bread_crumb">
+                <ul class="List_bread_crumb">
+                    <li class="home">
+                        <a href="#">
+                            <span>Trang chủ</span>
+                        </a>
+                        <span class="mr_lr">
+                            <i class="bi bi-chevron-right"></i>
+                        </span>
+                    </li>
+                    <li>
+                        <span class="name_prd">Câu hỏi thường gặp</span>
+                    </li>
+
+                </ul>
+            </div>
+        </section>
+        <section class="page_faq">
+            <div class="container_faq">
+                <div class="row">
+                    <div class="left_faq">
+                        <div class="content_page">
+                            <h3 class="faq_title">
+                                Hỏi đáp về tài khoản
+                            </h3>
+                            <div class="panel-group">
+                                <div class="faq active">
+                                    <h4>1. Làm thế nào để tôi trở thành thành viên của Dola?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng nhấn vào nút “Đăng ký” ở thanh menu trên cùng của màn hình
+                                            (Đối với Desktop) hoặc tại góc trái màn hình, chọn biểu tượng Menu rồi chọn
+                                            “Đăng ký” (Đối với Mobile).</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>2. Tại sao tôi không thể đăng nhập vào tài khoản của tôi?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng kiểm tra kỹ về kiểu gõ hoặc phím Caps Lock/ IN HOA trong
+                                            quá trình điền thông tin đăng nhập thành viên, trường hợp không thể đăng
+                                            nhập thành công quý khách vui lòng chọn nút “Quên mật khẩu” ngay dưới ô mật
+                                            khẩu và nhập email đã đăng ký.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>3. Tôi có thể sử dụng chung tài khoản với người khác được không?</h4>
+                                    <div class="content">
+                                        <p>Quý khách nên sử dụng tài khoản cá nhân để đảm bảo độ tin cậy cũng như quyền
+                                            lợi của bản thân khi mua sắm. Việc sử dụng chung tài khoản có thể dẫn đến
+                                            những sai sót mà người chịu ảnh hưởng trực tiếp chính là quý khách hàng.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>4. Tại sao tôi nên đăng ký thành viên Dola?</h4>
+                                    <div class="content">
+                                        <p>Việc đăng ký tài khoản là cơ hội giúp quý khách trở thành một trong những
+                                            khách hàng thân thiết của Dola, được tiếp cận nhanh nhất các chương trình
+                                            khuyến mãi, thông tin ưu đãi khi mua sắm.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>5. Dola có chương trình ưu đãi nào hấp dẫn dành cho khách hàng thân thiết?</h4>
+                                    <div class="content">
+                                        <p>Khi tổng giá trị đơn hàng của quý khách tích lũy đạt đủ điều kiện của từng
+                                            mức hạng thành viên, quý khách sẽ nhận được ưu đãi giảm giá cho mọi đơn hàng
+                                            tương, voucher sinh nhật tương ứng của hạng mức thành viên.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="faq_title">
+                                Hỏi đáp về tài khoản
+                            </h3>
+                            <div class="panel-group">
+                                <div class="faq active">
+                                    <h4>1. Làm thế nào để tôi trở thành thành viên của Dola?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng nhấn vào nút “Đăng ký” ở thanh menu trên cùng của màn hình
+                                            (Đối với Desktop) hoặc tại góc trái màn hình, chọn biểu tượng Menu rồi chọn
+                                            “Đăng ký” (Đối với Mobile).</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>2. Tại sao tôi không thể đăng nhập vào tài khoản của tôi?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng kiểm tra kỹ về kiểu gõ hoặc phím Caps Lock/ IN HOA trong
+                                            quá trình điền thông tin đăng nhập thành viên, trường hợp không thể đăng
+                                            nhập thành công quý khách vui lòng chọn nút “Quên mật khẩu” ngay dưới ô mật
+                                            khẩu và nhập email đã đăng ký.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>3. Tôi có thể sử dụng chung tài khoản với người khác được không?</h4>
+                                    <div class="content">
+                                        <p>Quý khách nên sử dụng tài khoản cá nhân để đảm bảo độ tin cậy cũng như quyền
+                                            lợi của bản thân khi mua sắm. Việc sử dụng chung tài khoản có thể dẫn đến
+                                            những sai sót mà người chịu ảnh hưởng trực tiếp chính là quý khách hàng.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>4. Tại sao tôi nên đăng ký thành viên Dola?</h4>
+                                    <div class="content">
+                                        <p>Việc đăng ký tài khoản là cơ hội giúp quý khách trở thành một trong những
+                                            khách hàng thân thiết của Dola, được tiếp cận nhanh nhất các chương trình
+                                            khuyến mãi, thông tin ưu đãi khi mua sắm.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>5. Dola có chương trình ưu đãi nào hấp dẫn dành cho khách hàng thân thiết?</h4>
+                                    <div class="content">
+                                        <p>Khi tổng giá trị đơn hàng của quý khách tích lũy đạt đủ điều kiện của từng
+                                            mức hạng thành viên, quý khách sẽ nhận được ưu đãi giảm giá cho mọi đơn hàng
+                                            tương, voucher sinh nhật tương ứng của hạng mức thành viên.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>5. Dola có chương trình ưu đãi nào hấp dẫn dành cho khách hàng thân thiết?</h4>
+                                    <div class="content">
+                                        <p>Khi tổng giá trị đơn hàng của quý khách tích lũy đạt đủ điều kiện của từng
+                                            mức hạng thành viên, quý khách sẽ nhận được ưu đãi giảm giá cho mọi đơn hàng
+                                            tương, voucher sinh nhật tương ứng của hạng mức thành viên.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <h3 class="faq_title">
+                                Hỏi đáp về tài khoản
+                            </h3>
+                            <div class="panel-group">
+                                <div class="faq active">
+                                    <h4>1. Làm thế nào để tôi trở thành thành viên của Dola?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng nhấn vào nút “Đăng ký” ở thanh menu trên cùng của màn hình
+                                            (Đối với Desktop) hoặc tại góc trái màn hình, chọn biểu tượng Menu rồi chọn
+                                            “Đăng ký” (Đối với Mobile).</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="faq_title">
+                                Hỏi đáp về tài khoản
+                            </h3>
+                            <div class="panel-group">
+                                <div class="faq active">
+                                    <h4>1. Làm thế nào để tôi trở thành thành viên của Dola?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng nhấn vào nút “Đăng ký” ở thanh menu trên cùng của màn hình
+                                            (Đối với Desktop) hoặc tại góc trái màn hình, chọn biểu tượng Menu rồi chọn
+                                            “Đăng ký” (Đối với Mobile).</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>2. Tại sao tôi không thể đăng nhập vào tài khoản của tôi?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng kiểm tra kỹ về kiểu gõ hoặc phím Caps Lock/ IN HOA trong
+                                            quá trình điền thông tin đăng nhập thành viên, trường hợp không thể đăng
+                                            nhập thành công quý khách vui lòng chọn nút “Quên mật khẩu” ngay dưới ô mật
+                                            khẩu và nhập email đã đăng ký.</p>
+                                    </div>
+                                </div>
+                                <div class="faq">
+                                    <h4>2. Tại sao tôi không thể đăng nhập vào tài khoản của tôi?</h4>
+                                    <div class="content">
+                                        <p>Quý khách vui lòng kiểm tra kỹ về kiểu gõ hoặc phím Caps Lock/ IN HOA trong
+                                            quá trình điền thông tin đăng nhập thành viên, trường hợp không thể đăng
+                                            nhập thành công quý khách vui lòng chọn nút “Quên mật khẩu” ngay dưới ô mật
+                                            khẩu và nhập email đã đăng ký.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right_faq">
+                        <div class="contacts">
+                            <h3>Giải đáp thách mắc</h3><br>
+                            <span class="content_form">Nếu bạn có thắc mắc gì, có thể gửi yêu cầu cho <br> chúng tôi, và
+                                chúng tôi sẽ liên lạc lại với bạn sớm <br> nhất có thể .</span>
+                            <div class="pagelogin">
+                                <form action="">
+                                    <div class="group_contact">
+                                        <input type="text" placeholder="Họ và tên">
+                                        <input type="email" placeholder="Email">
+                                        <input type="number" placeholder="Số điện thoại">
+                                        <textarea placeholder="Nội dung"></textarea>
+                                        <button type="submit">Gửi thông tin</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
