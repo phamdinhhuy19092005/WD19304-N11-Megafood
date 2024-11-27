dcf9943522a58c6b47d4b17b26808790add93920
Tuấn Anh <tanh7164@gmail.com>
2024-11-27 22:14:40 +0700

    nothing

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -31,7 +31,7 @@
 
         <ul class="category_list">
             <li class="category_item">
-                <a href="<?php echo BASE_URL; ?>index.php?route=products">
+                <a href="<?php echo BASE_URL; ?>index.php?route=detailCategories">
                     <p>Pizza <br>(17 món ăn) </p>
                     <img style="width: 104px; height: 105px;"
                         src="<?php echo IMG_BASE_URL; ?>category/category1.webp"
@@ -40,7 +40,7 @@
 
             </li>
             <li class="category_item">
-                <a href="<?php echo BASE_URL; ?>index.php?route=products">
+                <a href="<?php echo BASE_URL; ?>index.php?route=detailCategories">
                     <p>Khai vị <br>(13 món ăn) </p>
                     <img src="<?php echo IMG_BASE_URL; ?>category/category2.webp"
                         alt="">
@@ -48,14 +48,14 @@
 
             </li>
             <li class="category_item">
-                <a href="<?php echo BASE_URL; ?>index.php?route=products">
+                <a href="<?php echo BASE_URL; ?>index.php?route=detailCategories">
                     <p>Mỳ Ý <br>(7 món ăn) </p>
                     <img src="<?php echo IMG_BASE_URL; ?>category/category3.webp"
                         alt="">
                 </a>
             </li>
             <li class="category_item">
-                <a href="<?php echo BASE_URL; ?>index.php?route=products">
+                <a href="<?php echo BASE_URL; ?>index.php?route=detailCategories">
                     <p>Salad <br>(7 món ăn) </p>
                     <img src="<?php echo IMG_BASE_URL; ?>category/category4.webp"
                         alt="">
