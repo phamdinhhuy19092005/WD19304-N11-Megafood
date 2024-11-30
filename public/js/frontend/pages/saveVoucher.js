1473dc78f494c931c29f518fdcc78d86c5610576
NguyenDoTai <nguyendotai11@gmail.com>
2024-11-30 15:19:49 +0700

    update save voucher

--- public/js/frontend/pages/saveVoucher.js
+++ public/js/frontend/pages/saveVoucher.js
@@ -1,18 +1,29 @@
-//save vourcher
-
-document.querySelectorAll('.coupon_code').forEach((button, index) => {
+document.querySelectorAll('.coupon_code').forEach((button) => {
     button.addEventListener('click', function () {
         // Lấy nội dung mã voucher từ thẻ <b>
-        const textVoucher = button.parentElement.querySelector('b').innerText;
+        const textVoucher = button.closest('.content_voucher').querySelector('b') ? button.closest('.content_voucher').querySelector('b').innerText : '';
 
-        // Sao chép vào clipboard
-        navigator.clipboard.writeText(textVoucher)
-            .then(() => {
-                alert("Đã lưu mã voucher: " + textVoucher);
-            })
-            .catch(err => {
-                console.error('Không sao chép được: ', err);
-            });
+        if (textVoucher) {
+            // Sao chép vào clipboard
+            navigator.clipboard.writeText(textVoucher)
+                .then(() => {
+                    const notification_voucher = document.createElement("div");
+                    notification_voucher.className = "notification_voucher";
+                    notification_voucher.innerHTML = `
+                        <div class="notification_container">Đã lưu voucher: ${textVoucher}</div>
+                    `;
+                    document.body.appendChild(notification_voucher);
+                    notification_voucher.style.display = "block";
+
+                    setTimeout(() => {
+                        notification_voucher.style.display = "none"; // Ẩn thông báo sau 3 giây
+                    }, 3000);
+                })
+                .catch(err => {
+                    console.error('Không sao chép được: ', err);
+                });
+        } else {
+            console.error('Không tìm thấy mã voucher');
+        }
     });
 });
-
