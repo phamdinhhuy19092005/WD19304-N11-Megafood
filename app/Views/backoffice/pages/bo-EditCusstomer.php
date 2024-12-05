100156c6befd655b9d79a309f8eb5a63f1f8dc27
NguyenDoTai <nguyendotai11@gmail.com>
2024-12-05 15:39:50 +0700

    add edit customer

---
+++ app/Views/backoffice/pages/bo-EditCusstomer.php
@@ -0,0 +1,75 @@
+<div class="k-content">
+        <div class="k-content_head">
+            <div class="k-content_head-main">
+                <h3 class="k-content_head-title">Chỉnh sửa thông tin</h3>
+
+                <div class="k-content__head-breadcrumbs">
+                    <a href="" class="k-content__head-breadcrumb-home"><i class="fa-solid fa-house"></i></a>
+                    <a href="../html/customer.html" class="k-content__head-breadcrumb-link"><i class="fa-solid fa-circle"></i>Danh sách khách hàng</a>
+                    <a href="" class="k-content__head-breadcrumb-link"><i class="fa-solid fa-circle"></i>Chỉnh sửa thông tin khách hàng</a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <form action="" class="k-form">
+            <div class="k-form_body">
+                <div class="k-form_head">
+                    <div class="k-form_head-title">
+                        <h3>Chỉnh sửa thông tin khách hàng</h3>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="">Họ</label><br>
+                    <input type="text" id="firstName" class="form-control" placeholder="Nhập họ">
+                </div>
+                <div class="form-group">
+                    <label for="">Tên</label><br>
+                    <input type="text" id="lastName" class="form-control" placeholder="Nhập tên">
+                </div>
+                <div class="form-group">
+                    <label for="">E-mail</label><br>
+                    <input type="email" id="email" class="form-control" placeholder="Nhập E-mail">
+                </div>
+                <div class="form-group">
+                    <label for="">Số điện thoại</label><br>
+                    <input type="text" id="phone" class="form-control" placeholder="Nhập số điện thoại">
+                </div>
+                <div class="form-group-select">
+                    <div class="allow-login">
+
+                        <label for="">Cho phép đăng nhập*</label><br>
+                        <select class="form-control">
+                            <option value="1" selected>Có</option>
+                            <option value="2">Không</option>
+                        </select>
+                    </div>
+                    
+
+                    <div class="channel">
+
+                        <label for="">Kênh truy cập*</label><br>
+                        <select class="form-control">
+                            <option value="1" selected>Website</option>
+                            <option value="2">Shoppe</option>
+                            <option value="3">Lazada</option>
+                            <option value="4">Tiki</option>
+                            <option value="5">Facebook</option>
+                            <option value="6">Tiktok</option>
+
+                        </select>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="k-form_foot">
+                <div class="k-form_actions">
+                    <button type="submit" class="btn-save">Lưu</button>
+                    <button type="redirect" class="btn-cancel">Hủy</button>
+                    <button type="redirect" class="btn-delete">Xóa tài khoản</button>
+                </div>
+            </div>
+        </form>
+        
+    </div>
